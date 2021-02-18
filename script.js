3001740970eba4aee17ceb2aa1966e73828dfdaf
James Jordan <jpjordan1987@gmail.com>
2021-02-18 08:45:35 -0500

    Began a css file adding some formatting, will be switching from pregame to ingame to postgame views

--- script.js
+++ script.js
@@ -5,33 +5,33 @@
 
 //Thanks to Stack Overflow Question# 20618355
 
-window.onload = function () {
-    var oneMinute = 60,
-        display = document.querySelector('#time');
-    startTimer(oneMinute, display);
-};
+// window.onload = function () {
+//     var oneMinute = 60,
+//         display = document.querySelector('#time');
+//     startTimer(oneMinute, display);
+// };
 
-//Thanks to Stack Overflow Question# 20618355
-function startTimer(duration) {
-    var timer = document.getElementById("timer");
-    setInterval(function() {
-        if(time >= 0){
-            timer.textContent = "Time: " + time;
-            time--;
-        }
-    }, 1000);
-}
+// //Thanks to Stack Overflow Question# 20618355
+// function startTimer(duration) {
+//     var timer = document.getElementById("timer");
+//     setInterval(function() {
+//         if(time >= 0){
+//             timer.textContent = "Time: " + time;
+//             time--;
+//         }
+//     }, 1000);
+// }
 
-// Randomly picks guessword and formats guess box
-function setWord() {
-    var guessWord = guessWords[Math.floor(Math.random() * guessWords.length)];
-    for (var i = 0; i < guessWord.length; i++){
-        answerBox.innerHTML += "<p id='" + i + "element' data-value='" + guessWord[i] + "' data-status='hidden'></p>";
-    }
-    return guessWord;
-}
+// // Randomly picks guessword and formats guess box
+// function setWord() {
+//     var guessWord = guessWords[Math.floor(Math.random() * guessWords.length)];
+//     for (var i = 0; i < guessWord.length; i++){
+//         answerBox.innerHTML += "<p id='" + i + "element' data-value='" + guessWord[i] + "' data-status='hidden'></p>";
+//     }
+//     return guessWord;
+// }
 
-function setWord() {
-    var guessWord = guessWords[Math.floor(Math.random() * guessWords.length)];
-    return guessWord;
-}
+// function setWord() {
+//     var guessWord = guessWords[Math.floor(Math.random() * guessWords.length)];
+//     return guessWord;
+// }
